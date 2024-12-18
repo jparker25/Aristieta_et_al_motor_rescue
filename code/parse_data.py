f417555a667c1ec09f88aed3a70a7673d1e805f6
John Parker <jep9223@gmail.com>
2024-12-18 16:16:05 -0500

    Commented parse_data.py script.

--- code/parse_data.py
+++ code/parse_data.py
@@ -1,11 +1,21 @@
+"""
+parse_data.py
+
+Gathers and processes data for training and testing of the motor rescue model.
+
+Author: John E. Parker (2024)
+"""
+
 import numpy as np
-import json, sys
+import json
 
 # user modules
 import poisson_surprise
 from helpers import *
 
 #### GLOBAL VARIABLES ####
+
+# Paths to pre-processed data
 jaws_path = "/Users/johnparker/UPitt_Data/SNr_motor_rescue_project/jaws_pre_processed"
 npas_path = "/Users/johnparker/UPitt_Data/SNr_motor_rescue_project/npas_pre_processed"
 cont_gpe_data = (
@@ -36,10 +46,22 @@ dd_pulse_d1_data = (
     "/Users/johnparker/UPitt_Data/pulsed_light_terminal_20Hz/d1_msns/pre_processed_data"
 )
 
+# length of spike trains to analyze
 training_fixed_length = 30
 
 
 def read_in_meta_data(file_path):
+    """
+    Read in meta data from a file.
+
+    Parameters
+    ----------
+    \t file_path (str) : path read in meta data
+
+    Returns
+    -------
+    \t meta_data (dict) : dictionary of meta data
+    """
     meta_data = {}
     with open(file_path, "r") as file:
         for line in file.readlines():
@@ -49,11 +71,36 @@ def read_in_meta_data(file_path):
 
 
 def get_naive_baseline_data(length, *paths):
+    """
+    Get baseline spike trains for naive neurons.
+
+    Parameters
+    ----------
+    \t length (int) : length of baseline to analyze
+
+    \t paths (list) : list of paths to naive neuron data
+
+    Returns
+    -------
+    \t baseline_spike_trains (list) : list of baseline spike trains
+
+    \t baseline_lengths (list) : list of lengths of baseline spike trains
+
+    \t mouse (list) : list of mouse names
+
+    \t folder (list) : list of folder names
+
+    \t name (list) : list of channel names
+    """
+
+    # Initialize lists
     baseline_spike_trains = []
     baseline_lengths = []
     mouse = []
     folder = []
     name = []
+
+    # Loop through paths and grab spike train data
     for path in paths:
         for dir in os.listdir(path):
             if "Naive" in dir and not "hsyn" in dir:
@@ -85,11 +132,36 @@ def get_naive_baseline_data(length, *paths):
 
 
 def get_dd_baseline_data(length, *paths):
+    """
+    Get baseline spike trains for DD neurons.
+
+    Parameters
+    ----------
+    \t length (int) : length of baseline to analyze
+
+    \t paths (list) : list of paths to DD neuron data
+
+    Returns
+    -------
+    \t baseline_spike_trains (list) : list of baseline spike trains
+
+    \t baseline_lengths (list) : list of lengths of baseline spike trains
+
+    \t mouse (list) : list of mouse names
+
+    \t folder (list) : list of folder names
+
+    \t name (list) : list of channel names
+    """
+
+    # Initialize lists
     baseline_spike_trains = []
     baseline_lengths = []
     mouse = []
     folder = []
     name = []
+
+    # Loop through paths and grab spike train data
     for path in paths:
         for dir in os.listdir(path):
             if "6-OHDA" in dir and not "hsyn" in dir:
@@ -120,6 +192,13 @@ def get_dd_baseline_data(length, *paths):
 
 
 def save_naive_baseline_spikes(spikes):
+    """
+    Saves naive baseline spikes to file.
+
+    Parameters
+    ----------
+    \t spikes (list) : list of naive baseline spikes
+    """
     run_cmd("mkdir -p ../data/training/naive_neurons/spikes")
     for i in range(len(spikes)):
         np.savetxt(
@@ -132,6 +211,13 @@ def save_naive_baseline_spikes(spikes):
 
 
 def save_dd_baseline_spikes(spikes):
+    """
+    Saves DD baseline spikes to file.
+
+    Parameters
+    ----------
+    \t spikes (list) : list of DD baseline spikes
+    """
     run_cmd("mkdir -p ../data/training/dd_neurons/spikes")
     for i in range(len(spikes)):
         np.savetxt(
@@ -144,10 +230,34 @@ def save_dd_baseline_spikes(spikes):
 
 
 def calc_firing_rates(spikes, lengths):
+    """
+    Calculate firing rates for a list of spike trains.
+
+    Parameters
+    ----------
+    \t spikes (list) : list of spike trains
+
+    \t lengths (list) : list of lengths of spike trains
+
+    Returns
+    -------
+    \t firing_rates (np.array) : array of firing rates
+    """
     return np.asarray([spikes[i].shape[0] / lengths[i] for i in range(len(spikes))])
 
 
 def calc_cv(spikes):
+    """
+    Calculate coefficient of variation for a list of spike trains.
+
+    Parameters
+    ----------
+    \t spikes (list) : list of spike trains
+
+    Returns
+    -------
+    \t cvs (np.array) : array of coefficients of variation
+    """
     cvs = np.zeros(len(spikes))
     for i in range(len(spikes)):
         cv_spikes_diff = np.diff(spikes[i])
@@ -162,7 +272,32 @@ def calc_cv(spikes):
 def calc_burst_statistics(
     spikes, rates, lengths, min_spikes=3, surprise_threshold=3, window=0.25
 ):
+    """
+    Calculate burst statistics for a list of spike trains.
+
+    Parameters
+    ----------
+    \t spikes (list) : list of spike trains
+
+    \t rates (np.array) : array of firing rates
+
+    \t lengths (list) : list of lengths of spike trains
+
+    \t min_spikes=3 (int) : minimum number of spikes for a burst
+
+    \t surprise_threshold=3 (int) : surprise threshold for a burst
+
+    \t window=0.25 (float) : window for a burst
+
+    Returns
+    -------
+    \t burst_statistics (np.array) : array of burst statistics
+    """
+
+    # initialize burst statistics array
     burst_statistics = np.zeros((len(spikes), 10))
+
+    # iterate through spike trains and calculate burst statistics
     for i in range(len(spikes)):
         bursts = poisson_surprise.run_poisson_surprise(
             spikes[i],
@@ -179,15 +314,49 @@ def calc_burst_statistics(
 
 
 def save_stat(stat_data, path):
+    """
+    Save stat_data to file.
+
+    Parameters
+    ----------
+    \t stat_data (np.array) : array of data to save
+
+    \t path (str) : path to save data
+    """
     np.savetxt(path, stat_data, fmt="%f", newline="\n")
 
 
 def get_baseline_ephys_data(fixed_length, path):
+    """
+    Get baseline ephys data.
+
+    Parameters
+    ----------
+    \t fixed_length (int) : length of spike trains to analyze
+
+    \t path (str) : path to ephys data
+
+    Returns
+    -------
+    \t pre_spikes (list) : list of pre opto spike trains
+
+    \t is_medial (list) : list of medial boolean values
+
+    \t mouse (list) : list of mouse names
+
+    \t folder (list) : list of folder names
+
+    \t cell_name (list) : list of cell names
+    """
+
+    # Initialize lists
     pre_spikes = []
     is_medial = []
     mouse = []
     folder = []
     cell_name = []
+
+    # Iterate through files and generate data
     for neuron in sorted(os.listdir(f"{path}/pre_opto")):
         if neuron != ".DS_Store":
             meta_file = open(f"{path}/pre_opto/{neuron}/meta_data.txt", "r")
@@ -203,12 +372,39 @@ def get_baseline_ephys_data(fixed_length, path):
 
 
 def get_post_ephys_data(fixed_length, path):
+    """
+    Get post opto ephys data.
+
+    Parameters
+    ----------
+    \t fixed_length (int) : length of spike trains to analyze
+
+    \t path (str) : path to ephys data
+
+    Returns
+    -------
+    \t post_spikes (list) : list of post opto spike trains
+
+    \t post_times (list) : list of post opto times
+
+    \t is_medial (list) : list of medial boolean values
+
+    \t mouse (list) : list of mouse names
+
+    \t folder (list) : list of folder names
+
+    \t cell_name (list) : list of cell names
+    """
+
+    # initialize lists
     post_spikes = []
     post_times = []
     is_medial = []
     mouse = []
     folder = []
     cell_name = []
+
+    # Iterate through files and generate data
     for neuron in sorted(os.listdir(f"{path}/post_opto")):
         if neuron != ".DS_Store":
             meta_file = open(f"{path}/post_opto/{neuron}/meta_data.txt", "r")
@@ -225,7 +421,20 @@ def get_post_ephys_data(fixed_length, path):
 
 
 def save_motor_rescue_spikes(spikes, direc):
+    """
+    Save motor rescue spikes to file.
+
+    Parameters
+    ----------
+    \t spikes (list) : list of motor rescue spikes
+
+    \t direc (str) : directory to save spikes
+    """
+
+    # Create directory if it doesn't exist
     run_cmd(f"mkdir -p {direc}/spikes")
+
+    # Save spikes to file
     for i in range(len(spikes)):
         np.savetxt(
             f"{direc}/spikes/cell_{i:04d}.txt",
@@ -237,8 +446,23 @@ def save_motor_rescue_spikes(spikes, direc):
 
 
 def find_pre_post_matches(path):
+    """
+    Find pre/post matches.
+
+    Parameters
+    ----------
+    \t path (str) : path to ephys data
+
+    Returns
+    -------
+    \t pre_post_units (np.array) : array of pre/post units
+    """
+
+    # Initialize list of matches
     matches = ["mouse", "folder", "cell_name", "type"]
     pre_post_units = []
+
+    # Iterate through pre/post directories and find matches
     for pre_units in os.listdir(f"{path}/pre_opto"):
         if pre_units != ".DS_Store":
             pre_meta_file = open(f"{path}/pre_opto/{pre_units}/meta_data.txt", "r")
@@ -269,17 +493,45 @@ def find_pre_post_matches(path):
 
 
 def save_meta_list(stat_list, path):
+    """
+    Save meta list to file.
+
+    Parameters
+    ----------
+    \t stat_list (list) : list of meta data
+
+    \t path (str) : path to save meta data
+    """
     with open(path, "w") as f:
         for line in stat_list:
             f.write(f"{line}\n")
 
 
 def get_ephys_data(source_path, save_path, fixed_length):
+    """
+    Get ephys data.
+
+    Parameters
+    ----------
+    \t source_path (str) : path to ephys data
+
+    \t save_path (str) : path to save ephys data
+
+    \t fixed_length (int) : length of spike trains to analyze
+    """
+
+    # Create directories
     run_cmd(f"mkdir -p {save_path}")
     run_cmd(f"mkdir -p {save_path}/pre_opto")
     run_cmd(f"mkdir -p {save_path}/post_opto")
+
+    # Find pre/post matches
     pre_post_jaws = find_pre_post_matches(source_path)
+
+    # Save pre/post matches
     save_stat(pre_post_jaws, f"{save_path}/pre_post_units.txt")
+
+    # Get baseline ephys data
     (
         jaws_pre_spikes,
         jaws_pre_medial,
@@ -287,6 +539,8 @@ def get_ephys_data(source_path, save_path, fixed_length):
         jaws_pre_folder,
         jaws_pre_cell_name,
     ) = get_baseline_ephys_data(fixed_length, source_path)
+
+    # Get post opto ephys data
     (
         jaws_post_spikes,
         jaws_post_times,
@@ -296,6 +550,7 @@ def get_ephys_data(source_path, save_path, fixed_length):
         jaws_post_cell_name,
     ) = get_post_ephys_data(fixed_length, source_path)
 
+    # Calculate firing rates for jaws pre/post data
     jaws_pre_spikes_frs = calc_firing_rates(
         jaws_pre_spikes, np.ones(len(jaws_pre_spikes)) * fixed_length
     )
@@ -304,10 +559,12 @@ def get_ephys_data(source_path, save_path, fixed_length):
         jaws_post_spikes, np.ones(len(jaws_post_spikes)) * fixed_length
     )
 
+    # Calculate coefficient of variation for jaws pre/post data
     jaws_pre_spikes_cvs = calc_cv(jaws_pre_spikes)
 
     jaws_post_spikes_cvs = calc_cv(jaws_post_spikes)
 
+    # Calculate burst statistics for jaws pre/post data
     jaws_pre_burst_statistics = calc_burst_statistics(
         jaws_pre_spikes,
         jaws_pre_spikes_frs,
@@ -326,6 +583,7 @@ def get_ephys_data(source_path, save_path, fixed_length):
         window=2,
     )
 
+    # Save data
     save_motor_rescue_spikes(jaws_pre_spikes, f"{save_path}/pre_opto")
     save_motor_rescue_spikes(jaws_post_spikes, f"{save_path}/post_opto")
 
@@ -366,6 +624,16 @@ def get_ephys_data(source_path, save_path, fixed_length):
 
 
 def create_ephys_training_data(fixed_length):
+    """
+    Create ephys training data.
+
+    Parameters
+    ----------
+
+    \t fixed_length (int) : length of spike trains to analyze
+    """
+
+    # Create directories
     save_dir = "../data/training"
 
     get_ephys_data(jaws_path, f"{save_dir}/jaws_neurons", fixed_length)
@@ -377,13 +645,22 @@ def create_ephys_training_data(fixed_length):
 
 
 def create_pulse_cont_training_data(fixed_length):
+    """
+    Create pulse/cont training data.
+
+    Parameters
+    ----------
+    \t fixed_length (int) : length of spike trains to analyze
+    """
+
+    # Create directories
     save_dir = "../data/training"
     run_cmd(f"mkdir -p {save_dir}")
     run_cmd(f"mkdir -p {save_dir}/naive_neurons")
     run_cmd(f"mkdir -p {save_dir}/dd_neurons")
 
+    # Check if pulse data exists and generate spike train data
     pulse = True
-
     if pulse:
         (
             naive_baseline_spikes,
@@ -421,6 +698,7 @@ def create_pulse_cont_training_data(fixed_length):
             )
         )
 
+    # Calculate firing rates, CVs, and burst statistics
     naive_frs = calc_firing_rates(naive_baseline_spikes, naive_baseline_lengths)
     dd_frs = calc_firing_rates(dd_baseline_spikes, dd_baseline_lengths)
 
@@ -445,6 +723,7 @@ def create_pulse_cont_training_data(fixed_length):
         window=2,
     )
 
+    # Save data
     save_dd_baseline_spikes(dd_baseline_spikes)
     save_naive_baseline_spikes(naive_baseline_spikes)
     save_stat(
@@ -481,6 +760,7 @@ def create_pulse_cont_training_data(fixed_length):
     )
 
 
-# Create data
-# create_ephys_training_data(training_fixed_length)
-# create_pulse_cont_training_data(training_fixed_length)
+# Create data and generate statistics for further analysis
+
+create_ephys_training_data(training_fixed_length)
+create_pulse_cont_training_data(training_fixed_length)
